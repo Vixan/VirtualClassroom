990eba3e3e84c9cd4947c3d55a79a851fed20242
Petru Grozea <petrugrozea@gmail.com>
2018-04-20 15:58:55 +0300

    add business interfaces

---
+++ VirtualClassroom.Core.Shared/IProfessorLogic.cs
@@ -0,0 +1,15 @@
+﻿using VirtualClassroom.Domain;
+
+namespace VirtualClassroom.Core.Shared
+{
+    public interface IProfessorLogic
+    {
+        bool CreateActivity(int professorId, Activity activity);
+
+        bool EditActivity(int professorId, Activity activity);
+
+        bool DeleteActivity(int professorId, int activityIdentifier);
+
+        Activity GetActivity(int professorIdentifier, int activityIdentifier);
+    }
+}
---
+++ VirtualClassroom.Core/ProfessorServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using VirtualClassroom.Core.Shared;
+using VirtualClassroom.Domain;
+
+namespace VirtualClassroom.Core
+{
+    class ProfessorServices : IProfessorLogic
+    {
+        public bool CreateActivity(int professorIdentifier, Activity activity)
+        {
+            // get professor from DB
+            Professor professor;
+
+            //professor.
+
+            throw new NotImplementedException();
+        }
+
+        public bool EditActivity(int professorIdentifier, Activity activity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteActivity(int professorIdentifier, int activityIdentifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Activity GetActivity(int professorIdentifier, int activityIdentifier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
