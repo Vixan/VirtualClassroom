7a7eb2bb49b48bd2f29b9dc6ea10c77f0ce1560d
Petru Grozea <petrugrozea@gmail.com>
2018-04-20 15:58:55 +0300

    add business interfaces

---
+++ VirtualClassroom.Core.Shared/IStudentLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using VirtualClassroom.Domain;
+
+namespace VirtualClassroom.Core.Shared
+{
+    public interface IStudentLogic
+    {
+        IEnumerable<Activity> ViewActivities(Student student);
+
+        IEnumerable<int> ViewActivityGrades(int studentIdentifier, int activityIdentifier);
+
+        IEnumerable<bool> ViewActivityAttendance(int studentIdentifier, int activityIdentifier);
+    }
+}
---
+++ VirtualClassroom.Core/StudentServices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using VirtualClassroom.Core.Shared;
+using VirtualClassroom.Domain;
+
+namespace VirtualClassroom.Core
+{
+    public class StudentServices : IStudentLogic
+    {
+        // private IPersistenceContext persistanceContext
+
+        public IEnumerable<Activity> ViewActivities(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<bool> ViewActivityAttendance(int studentIdentifier, int activityIdentifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> ViewActivityGrades(int studentIdentifier, int activityIdentifier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
