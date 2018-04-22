79b72966a567ede0463ed840fbcfabbfb6a2ee75
Petru Grozea <petrugrozea@gmail.com>
2018-04-22 23:11:22 +0300

    change ProfessorServices Edit method

--- VirtualClassroom.Core/ProfessorServices.cs
+++ VirtualClassroom.Core/ProfessorServices.cs
@@ -6,23 +6,43 @@ namespace VirtualClassroom.Core
 {
     class ProfessorServices : IProfessorLogic
     {
+        // private IPersistanceContext persistanceContext;
+
         public bool CreateActivity(int professorIdentifier, Activity activity)
         {
-            // get professor from DB
+            
             Professor professor = new Professor();
 
             professor.Activities.Add(activity);
 
             // save changes
 
-            throw new NotImplementedException();
+            return true;
         }
 
         public bool EditActivity(int professorIdentifier, Activity activity)
         {
+            // get professo from Db
+            Professor professor = new Professor();
+            Activity activity_to_edit = null;
+            
+            foreach(var professor_activity in professor.Activities)
+            {
+                if(professor_activity.Id == activity.Id)
+                {
+                    activity_to_edit = professor_activity;
+                    break;
+                }
+            }
 
+            if (activity_to_edit == null)
+                return false;
 
-            throw new NotImplementedException();
+            activity_to_edit = activity;
+
+            // save changes
+
+            return true;
         }
 
         public bool DeleteActivity(int professorIdentifier, int activityIdentifier)
